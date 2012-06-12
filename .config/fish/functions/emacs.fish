3be814d6536f85fd5449c1989d95e371085907f6
Lars Kinn Ekroll <larskinn@gmail.com>
2012-06-13 01:14:01 +0200

    Adds emacs and fish configuration

---
+++ .config/fish/functions/emacs.fish
@@ -0,0 +1,3 @@
+function emacs
+	/usr/local/Cellar/emacs/24.1/Emacs.app/Contents/MacOS/Emacs -nw
+end
---
+++ .emacs.d/init.el
@@ -0,0 +1,69 @@
+;; Requirements
+(require 'package)
+
+;;; ======================
+;;; Package manager config
+;;; ======================
+(setq package-list '(clojure-mode paredit rainbow-delimiters
+		     markdown-mode adaptive-wrap))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+;; fetch list of available packages
+(when (not package-archive-contents)
+  (package-refresh-contents))
+; install the missing packages
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+;;; ===============
+;;; Global settings
+;;; ===============
+
+;; Backup files
+(defvar backup-dir "~/.emacs-saves")
+(make-directory backup-dir t) ; create dir if it doesn't exist
+(setq backup-directory-alist `(("." . ,backup-dir)))
+
+;; Color and highlight parens
+(global-rainbow-delimiters-mode)
+(show-paren-mode)
+
+;; OS X-specific key bindings
+(if (eq window-system 'ns) ;ns for Nextstep
+  (progn
+    (setq mac-command-modifier 'meta)
+    (setq mac-control-modifier 'control)
+    (setq mac-option-modifier nil)))
+
+;; Interface options
+(line-number-mode)
+(column-number-mode)
+(load-theme 'wombat)
+
+;; Word wrap
+(global-visual-line-mode)
+
+;;; ======================
+;;; Mode-specific settings
+;;; ======================
+
+;;; Text
+;;; ----
+(add-hook 'text-mode-hook 'adaptive-wrap-prefix-mode)
+
+;;; Markdown
+;;; --------
+(add-hook 'markdown-mode-hook 'adaptive-wrap-prefix-mode)
+
+;;; Clojure
+;;; -------
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
